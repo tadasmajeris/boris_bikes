a35aa4973330654e000a29f6f9139e59e82ab4cb
souljuse <oppo.irene@gmail.com>
2016-09-26 17:59:36 +0200

    method release_bike added and updated test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,6 @@
 require "docking_station"
 describe DockingStation do
-
+    it "should respond to the method release_bike" do
+      expect(DockingStation.new).to respond_to :release_bike
+    end
 end
