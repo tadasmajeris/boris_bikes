d994d33d54ed29366916bcd9adaab28f93219f37
amidabrian51 <briankaty1@me.com>
2016-09-27 17:36:09 +0100

    We raised exceptions

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,7 +4,8 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' if @bike.nil?
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ describe DockingStation do
  it { is_expected.to respond_to :release_bike}
 
  it 'releases bike and checks if its working' do
-   bike = subject.release_bike
+   bike = Bike.new
    expect(bike).to be_working
  end
 
@@ -20,5 +20,16 @@ describe DockingStation do
    bike3 = Bike.new
    subject.dock(bike3)
    expect(subject.bike).to eq bike3
+ end
+
+ describe '#release_bike' do
+   it 'It should raise an error if the docking_station is empty' do
+     expect { subject.release_bike }.to raise_error 'No bikes available'
+   end
+   it 'returns docked bike' do
+     bike3 = Bike.new
+     subject.dock(bike3)
+     expect(subject.release_bike).to eq bike3
+   end
  end
 end
