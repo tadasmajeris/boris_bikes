262d40d22eef64f4241503ad8207ac807da482f5
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-27 16:34:33 +0100

    Added more tests. it now docks the bike, and checks that its docked

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,24 @@
 require "docking_station"
 describe DockingStation do
  it { is_expected.to respond_to :release_bike}
+
  it 'releases bike and checks if its working' do
    bike = subject.release_bike
    expect(bike).to be_working
+ end
+
+ it { is_expected.to respond_to(:dock).with(1).argument }
+
+ it { is_expected.to respond_to :bike }
+
+ it 'docks something' do
+   bike2 = Bike.new
+   expect(subject.dock(bike2)).to eq bike2
+ end
+
+ it 'returns docked bikes' do
+   bike3 = Bike.new
+   subject.dock(bike3)
+   expect(subject.bike).to eq bike3
  end
 end
