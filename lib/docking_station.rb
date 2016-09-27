7807c9fa7f8d7328e67a9f446f7323d62dc824d6
Chetan Sanghani <crsanghani@gmail.com>
2016-09-27 18:09:24 +0100

    half way through 12 :broken working bikes test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    @bike
   end
 
   def dock(bike)
-
+    @bike = bike
   end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,26 @@ describe DockingStation do
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'docks something' do
+
+    bike = Bike.new
+
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+
+    expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
 end
