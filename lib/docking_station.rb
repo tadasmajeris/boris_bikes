bf5951f92bf2f9099a1f504328612ba947db563f
Liam Taylor <liam.taylor888@googlemail.com>
2016-09-28 12:46:45 +0100

    Our personal docking station: If there is no bike, allows bike to be docked, if there is one, bike can be released

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,9 +3,9 @@ require_relative 'bike'
 class DockingStation
 	def release_bike
     raise "Oh no! No bikes available!" unless @bike
-    @bike
 	end
 	def dock(bike)
+		raise "Oh no! There's already a bike here!" if @bike
 		@bike = bike
 	end
 	attr_reader :bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,14 +3,14 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  #it "produces working bikes" do
-  #  red = subject.release_bike
-  #  expect(red).to be_working
-  #end
-  it "docks something" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
+#  it "produces working bikes" do
+#    red = subject.release_bike
+#    expect(red).to be_working
+#  end
+#  it "docks something" do
+#    bike = Bike.new
+#    expect(subject.dock(bike)).to eq bike
+#  end
 
   describe '#release_bike' do
     it 'releases a bike' do
@@ -22,5 +22,15 @@ describe DockingStation do
       expect {subject.release_bike}.to raise_error ("Oh no! No bikes available!")
     end
   end
+
+  describe '#dock' do
+    it 'does not accept more than one bike' do
+      #  bike = @bike
+      #    expect {subject.dock(bike) }.to raise_error ("Oh no! There's already a bike here!")
+      subject.dock(Bike.new)
+      expect { sucject.dock Bike.new }.to raise_error "Oh no! There's already a bike here!"
+    end
+  end  
+
 
 end
