4ebd65e9ea08e8d824d4f6dde03e588ee9365829
Liam Taylor <liam.taylor888@googlemail.com>
2016-09-28 10:21:43 +0100

    Bike instance variable and docking method created

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,13 @@
 require 'docking_station'
-
 describe DockingStation do
 it { is_expected.to respond_to :release_bike }
-
+it { is_expected.to respond_to(:dock).with(1).argument }
 it "produces working bikes" do
   red = subject.release_bike
   expect(red).to be_working
   end
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
 end
-
