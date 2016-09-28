83be4024836b3949f6204b73e37948c058812745
Stephen Gregory <stejgregory@gmail.com>
2016-09-28 12:21:09 +0100

    Created errors for lack of bike. Now to create 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,10 +4,12 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
+    fail "No bikes available" unless @bike
     @bike
   end
 
   def dock(bike)
+    fail 'Docking station is full' if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,10 @@ describe DockingStation do
 
   it {is_expected.to respond_to :release_bike}
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -25,11 +25,17 @@ describe DockingStation do
     expect(subject.bike).to eq bike
   end
 
+  describe '#dock' do
+    it 'raises an error when a bike is already in the dock' do
+      subject.dock(Bike.new)
+      expect {subject.dock Bike.new}.to raise_error "Docking station is full"
+    end
+  end
+
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+    it 'raises an error when no bikes are available' do
+      # Reminds you that subject is = to DockingStation.new
+      expect {subject.release_bike}.to raise_error "No bikes available"
     end
   end
 end
