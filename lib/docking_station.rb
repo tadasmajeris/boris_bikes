fb09817f6b72abe4e2ba1f9e21da75835918c6d9
souljuse <oppo.irene@gmail.com>
2016-09-28 18:15:00 +0200

    docking station does not accept more than 20 bikes - step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+    @twentybikes = []
+  end
 	def release_bike
-    raise "Oh no! No bikes available!" unless @bike
-    @bike
+    raise "Oh no! No bikes available!" if @twentybikes.empty?
+    @twentybikes.pop
 	end
 	def dock(bike)
-		@bike = bike
+    #if @twentybikes.length < 20
+    #    @twentybikes.push(bike)
+    #else
+    #  "no more bikes"
+    #end
+    fail 'Docking station full' if @twentybikes.count >= 20
+		@twentybikes << bike
 	end
 	attr_reader :bike
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,7 +9,7 @@ describe DockingStation do
   #end
   it "docks something" do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
   describe '#release_bike' do
@@ -20,6 +20,10 @@ describe DockingStation do
     end
     it "should not release infinite bikes" do
       expect {subject.release_bike}.to raise_error ("Oh no! No bikes available!")
+    end
+    it "should not accept more than 20 bikes" do
+      20.times { subject.dock Bike.new}
+      expect { subject.dock Bike.new}.to raise_error ("Docking station full")
     end
   end
 
