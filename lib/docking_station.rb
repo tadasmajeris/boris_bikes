0eee5c7823c7c7dbd6b36eb2417670cd18569a0f
hanrattyjen <hanratty.jen@gmail.com>
2016-09-28 13:11:51 +0100

    Changed the unit test to allow for a bike capacity of 20.
    Created the @bikes array. Refactored docking_station.rb.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    fail "No bikes available" unless @bike
-    @bike
+    fail "No bikes available" if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bike
-    @bike = bike
+    fail 'Docking station is full' if @bikes.count >= 20
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,24 +11,24 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'docks something' do
-
-    bike = Bike.new
-
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-
-    expect(subject.bike).to eq bike
-  end
+  # it 'docks something' do
+  #
+  #   bike = Bike.new
+  #
+  #   expect(subject.dock(bike)).to eq bike
+  # end
+  #
+  # it 'returns docked bikes' do
+  #   bike = Bike.new
+  #   subject.dock(bike)
+  #
+  #   expect(subject.bike).to eq bike
+  # end
 
   describe '#dock' do
     it 'raises an error when a bike is already in the dock' do
-      subject.dock(Bike.new)
-      expect {subject.dock Bike.new}.to raise_error "Docking station is full"
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error "Docking station is full"
     end
   end
 
