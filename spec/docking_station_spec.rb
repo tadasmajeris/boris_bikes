296f303487f2e19b20169695c5501763f1804cf5
hanrattyjen <hanratty.jen@gmail.com>
2016-09-29 09:58:03 +0100

    Completed exercise 14. 'docks something' test passed.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,17 +12,13 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'docks something' do
-
     bike = Bike.new
-    puts "bike #{bike}"
-    puts "subject.dock #{subject.dock(bike)}"
-    expect(subject.dock(bike)).to eq @bikes
+    expect(subject.dock(bike)).to eq bike
     end
 
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-
     expect(subject.release_bike).to eq bike
   end
 
