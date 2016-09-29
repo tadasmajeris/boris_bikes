16b5eb088363c70c94ff48e5527cd8d80ddaaa01
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-29 10:05:51 +0100

    Refactored the tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,28 +4,28 @@ describe DockingStation do
 
   it {is_expected.to respond_to :release_bike}
 
-  # it 'releases working bikes' do
-  #   bike = subject.release_bike
-  #   expect(bike).to be_working
-  # end
-
   it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-    end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq bike
-  end
 
   describe '#dock' do
     it 'raises an error when a bike is already in the dock' do
       20.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error "Docking station is full"
+    end
+
+    bike = Bike.new
+
+    it 'releases working bikes' do
+      bike = subject.dock(bike)
+      expect(bike).to be_working
+    end
+
+    it 'docks something' do
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
     end
   end
 
