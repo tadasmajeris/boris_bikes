f61502b3790465f8c4bca01ff883548ac3239065
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-29 10:13:34 +0100

    step 15. private methods full? empty?

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,14 +7,23 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes available" if @bikes.empty?
+    fail "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bikes.count >= 20
+    fail 'Docking station is full' if full?
     @bikes << bike
     @bikes.last
   end
 
+  private
+
+  def empty?
+    @bikes.empty?
+  end
+
+  def full?
+    @bikes.count >= 20
+  end
 end
